d89e437648c8f66494ff30abc38ebc2d1f10ce89
ZeroDegree0123 <zachary.vasquez.w@gmail.com>
2022-01-23 12:45:31 -0800

    Establishes game preview model and game review model

--- models/game.js
+++ models/game.js
@@ -2,7 +2,58 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+const gameReviewSchema = new Schema({
+    message: String,
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        default: undefined
+    },
+    user: {type: Schema.Types.ObjectId, ref: 'User'},
+    userName: String,
+    userAvatar: String
+}, {
+  // Will add and maintain
+  // createdAt & updatedAt properties
+    timestamps: true
+});
 
+const gameSchema = new Schema({
+    image: String,
+    title: {
+        type: String,
+        required: true
+    },
+    releaseYear: {
+        type: Number,
+    },
+    mpaaRating: {
+        type: String,
+        enum: ['eC', 'E', 'E 10+', 'T', 'M', 'A', 'RP']
+    },
+    maker: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Company'
+      }],
+    reviews: [gameReviewSchema],
+    platforms: {
+        type: String,
+        enum: ['Xbox', 'Playstation', 'Nintendo', 'PC', 'VR']
+    },
+    crossPlatform: {
+        type: Boolean,
+        default: false
+    },
+    multiplayer: {
+        type: String,
+        enum: ['Singleplayer', 'Co-Operative Singleplayer', 'MMO', 'Multiplayer']
+    },
+    genre: {
+        type: String,
+        enum: ['Action', 'Adventure & Casual', 'Role-Playing', 'Simulation', 'Stragety', 'Sandbox', 'Sports', ]
+    }
+})
 
 
 
