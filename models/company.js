80443afda4009c794e79869794cd11659021da74
ZeroDegree0123 <zachary.vasquez.w@gmail.com>
2022-01-23 12:54:58 -0800

    Establishes company model, though it's subject to change

---
+++ models/company.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const companySchema = new Schema({
+  title: { 
+      type: String
+    },
+  founded: { 
+      type: Number, 
+      min: 0 
+    },
+  games: { 
+      type: Schema.Types.ObjectId, 
+      ref: "Game" },
+});
+
+module.exports = mongoose.model("Company", ticketSchema);
